74a27df129a664035cae26cb8410389384f10bca
D-Bars <awotik26@gmail.com>
2024-10-29 16:31:42 +0100

    added services block

--- front-page.php
+++ front-page.php
@@ -42,46 +42,84 @@
                 <div class="why__us__item__content">
                     <i class="fa-solid fa-globe"></i>
                     <div class="why__us__slider__title">Translation Read</div>
-                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet</div>
+                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet</div>
                 </div>
             </div>
             <div class="why__us__slider__item">
                 <div class="why__us__item__content">
                     <i class="fa-solid fa-print"></i>
                     <div class="why__us__slider__title">Demos Included</div>
-                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet</div>
+                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet</div>
                 </div>
             </div>
             <div class="why__us__slider__item">
                 <div class="why__us__item__content">
                     <i class="fa-solid fa-feather"></i>
                     <div class="why__us__slider__title">Usefull Features</div>
-                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet</div>
+                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit.Lorem ipsum dolor sit amet</div>
                 </div>
             </div>
             <div class="why__us__slider__item">
                 <div class="why__us__item__content">
                     <i class="fa-solid fa-tv"></i>
                     <div class="why__us__slider__title">Unlimited Colors</div>
-                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet</div>
+                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit.Lorem ipsum dolor sit amet</div>
                 </div>
             </div>
             <div class="why__us__slider__item">
                 <div class="why__us__item__content">
                     <i class="fa-solid fa-palette"></i>
                     <div class="why__us__slider__title">Minimalist Design</div>
-                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet</div>
+                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit.Lorem ipsum dolor sit amet</div>
                 </div>
             </div>
             <div class="why__us__slider__item">
                 <div class="why__us__item__content">
                     <i class="fa-solid fa-building"></i>
                     <div class="why__us__slider__title">Page Builder</div>
-                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet</div>
+                    <div class="why__us__slider__description">Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit.Lorem ipsum dolor sit amet</div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="services__block container-fluid">
+    <div class="services__box row">
+        <div class="services__item col-12">
+            <div class="services__wrapper__img"><img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/services1.jpg" alt="Services"></div>
+            <div class="services__content">
+                <span>WE'RE HAZEL</span>
+                <div class="services__content__title">100% <span>RESPONSIVE
+                        <br />FROM HD TO MOBILE</span>
+                </div>
+                <div class="services__content__description">Globally evisculate synergistic niche markets whereas timely
+                    e-markets. Distinctively formulate timely web-readiness with long-term high-impact infrastructures.
+                </div>
+                <button class="services__btn">Buy Hazel</button>
+            </div>
+        </div>
+        <div class="services__item reverse col-12">
+            <div class="services__wrapper__img"><img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/services2.jpg" alt="Services"></div>
+            <div class="services__content">
+                <span>WE HAVE ALL</span>
+                <div class="services__content__title">BUILD AN AWESOME <br /><span>
+                        WEBSITE WITH HAZEL</span>
+                </div>
+                <div class="services__content__description">Globally evisculate synergistic niche markets whereas timely e-markets. Distinctively formulate timely web-readiness with long-term high-impact infrastructures.</div>
+                <button class="services__btn">see features</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php get_footer(); ?>

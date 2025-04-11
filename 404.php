61f4c8c1234625ccffa015cdd8bf596c7a51b4f1
D-Bars <awotik26@gmail.com>
2025-04-11 23:31:08 +0200

    added: 404 page

--- 404.php
+++ 404.php
@@ -1,4 +1,12 @@
+<?php get_header('empty'); ?>
 <div class="error__page__block">
-    <i class="fa-regular fa-face-frown"></i>
-    <div class="oops">OOOOOOPS</div>
+    <div class="error__page__wrapper__img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.svg" alt="Error404">
+    </div>
+    <div class="error__page__message">
+        <span><?php _e('Sry, this page not found', 'hazel'); ?></span>
+        <a class="error__page__home__btn" href="<?php echo home_url(); ?>"><?php _e('Home', 'hazel'); ?></a>
+    </div>
 </div>
+
+<?php get_footer('empty'); ?>
